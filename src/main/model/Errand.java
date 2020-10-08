ff5d54354c971e481a4a22e1f3113966d4d5f115
Devam Sisodraker <devam@sisodraker.com>
2020-10-08 15:53:31 -0700

    Added an errand class

---
+++ src/main/model/Errand.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.time.LocalDate;
+
+/**
+ * Read `Task` description and familiarize yourself with the concept before you read
+ * the description for an `Errand`.
+ *
+ * Errands are small things the user has to do, some examples:
+ *  - I have to renew my passport before next week
+ *  - I need to buy more groceries before the end of the month
+ *  - I need to take my dog on a walk today
+ *
+ * Some examples of what an errand isn't:
+ *  - I need to finish half of my webwork
+ *  - I need to review my essay before I hand it in
+ *  - I have to finish a Canvas quiz today
+ *
+ * As mentioned in the `Task` description, sometimes users will have tasks that are
+ * staged and completed differently. An `Errand` is a specific kind of task where
+ * staging moves from To-Do to Done Instantly.
+ */
+public class Errand extends Task {
+
+    private boolean hasBeenCompleted;
+
+    // EFFECTS: Creates a new untitled errand which has not been completed
+    public Errand() {
+        super("Untitled Errand");
+        this.hasBeenCompleted = false;
+    }
+
+    // EFFECTS: Creates a new errand with the given title which has not been completed
+    public Errand(String title) {
+        super(title);
+        this.hasBeenCompleted = false;
+    }
+
+    // EFFECTS: Creates a new errand with the given title, and complete by date,
+    //          which has not been completed
+    public Errand(String title, LocalDate completeByDate) {
+        super(title, "", completeByDate);
+        this.hasBeenCompleted = false;
+    }
+
+    // EFFECTS: Returns the percentage of task completion as a floating point number
+    //          within the domain [0,1]
+    @Override
+    public float percentageComplete() {
+        if (this.hasBeenCompleted) {
+            return 1f;
+        }
+        return 0f;
+    }
+
+
+
+}

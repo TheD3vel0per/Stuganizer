c34697e09b78f14229306eafd31550fada00cbfd
Devam Sisodraker <devam@sisodraker.com>
2020-10-30 16:01:58 -0700

    Added more documentation

--- src/main/persistence/JsonifiableArray.java
+++ src/main/persistence/JsonifiableArray.java
@@ -3,6 +3,14 @@ package persistence;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * This is an interface which can be used to turn any list type into a JSON array.
+ *
+ * It is required to be used for any array, list, or collection type where you
+ * want any sort of data to persist.
+ *
+ * Implement the interface and write out the appropriate methods.
+ */
 public interface JsonifiableArray {
     public JSONArray toJson();
 }
--- src/main/persistence/JsonifiableObject.java
+++ src/main/persistence/JsonifiableObject.java
@@ -2,6 +2,14 @@ package persistence;
 
 import org.json.JSONObject;
 
+/**
+ * This is an interface which can be used to turn any list type into a JSON object.
+ *
+ * It is required to be used for any object type where you
+ * want any sort of data to persist.
+ *
+ * Implement the interface and write out the appropriate methods.
+ */
 public interface JsonifiableObject {
     public JSONObject toJson();
 }
